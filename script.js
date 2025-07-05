2945cc929a1586cd79704e1d215aa4e76c5b1cf1
aa89976566 <aa89976566@gmail.com>
2025-07-06 00:56:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,188 +1,65 @@
-window.onload = function() {
-  const cvs = document.querySelector(".cnvs");
-  const size = 110;
-  cvs.width = size;
-  cvs.height = size;
-  const ctx = cvs.getContext("2d");
-  const pxSize = 2;
-  const defaultColor = "rgb(60, 75, 60)";
-  const sleepingCat = `
-xx...xx........x
-x.xxx.x........x
-x.....x.......x.
-x.x.x.xxxxx..x..
-x.....x....xxx..
-.x...x.....x....
-..xxxx.....x....
-....xxxxxxx.....
-`;
+body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+}
+.wrapper {
+  width: 100%;
+  height: 100%;
+  margin-top: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  const standingCatToLeft = `
-xx...xx........x
-x.xxx.x........x
-x.....x.......x.
-x.x.x.xxxxx..x..
-x.....x....xxx..
-.x...x.....x....
-..xxxx.....x....
-....xxxxxxx.....
-.....x.x.xxx....
-.....x.x.x.x....
-`;
+.innerWrapper {
+  width: 250px;
+  position: relative;
+}
 
-  const standingCatToLeftPose2 = `
-xx...xx........x
-x.xxx.x........x
-x.....x.......x.
-x.x.x.xxxxx..x..
-x.....x....xxx..
-.x...x.....x....
-..xxxx.....x....
-....xxxxxxx.....
-....x.x.xxx.....
-...x..x.x..x....
-`;
+svg {
+  width: 250px;
+}
 
-  const sleepZSml = `
-xx
-.x.
-.xx
-`;
+.cnvs {
+  background-color: rgba(60, 75, 60, 0.3);
+  position: absolute;
+  top: 108px;
+  left: 70px;
+  border-radius: 10px;
+  box-shadow: 5px 4px 0px 1px rgba(40,55,40, 0.4) inset;
+}
 
-  const sleepZMed = `
-xxx.
-..x.
-.x..
-.xxx
-`;
+.btn-1 {
+  position: absolute;
+  top: 257px;
+  left: 80px;
+}
 
-  const sleepZLarge = `
-xxxx.
-..x..
-.x...
-.xxxx
-`;
+.btn-2 {
+  position: absolute;
+  top: 265px;
+  left: 120px;
+}
 
-  let state = {
-    type: "walk",
-    x: 0
-  };
+.btn-3 {
+  position: absolute;
+  top: 257px;
+  left: 160px;
+}
 
-  function drawArt(px, py, str, flip, col = "rgb(60, 75, 60)") {
-    const lines = str.split("\n");
-    for (let y = 0; y < lines.length; y++) {
-      let chars = lines[y].split("");
-      if (flip) chars.reverse();
-      for (let x = 0; x < chars.length; x++) {
-        if (chars[x] === "x") drawRect(x + px, y + py, col, true);
-      }
-    }
-  }
+button:focus {outline:0;}
 
-  function drawRect(x, y, col = "black", fill = false) {
-    if (fill) {
-      ctx.fillStyle = col;
-      ctx.fillRect(x * pxSize, y * pxSize, pxSize, pxSize);
-      return;
-    }
-    ctx.strokeStyle = col;
-    ctx.fillStyle = "";
-    ctx.beginPath();
-    ctx.lineWidth = "1";
-    ctx.rect(x * pxSize, y * pxSize, pxSize, pxSize);
-    ctx.stroke();
-  }
+.btn {
+  border: none;
+  background-color: #fdffdf;
+  width: 20px;
+  height: 20px;
+  border-radius: 100%;
+  box-shadow: 1px 1px 0px 2px #fdffdf44;
+}
 
-  function clear() {
-    ctx.clearRect(0, 0, size, size);
-  }
-
-  function refresh() {
-    clear();
-    switch (state.type) {
-      case "sleep":
-        sleepAnimation();
-        break;
-      case "stand":
-        standAnimation();
-        break;
-      case "walk":
-        walkAnimation();
-        break;
-      default:
-    }
-  }
-
-  function sleepAnimation() {
-    if (state.t === undefined) state.t = 0;
-    drawArt(20, 32, sleepingCat);
-    if (state.t >= 1) drawArt(28, 27, sleepZSml);
-    if (state.t >= 2) drawArt(32, 22, sleepZMed);
-    if (state.t >= 3) drawArt(36, 18, sleepZLarge);
-    state.t = (state.t + 1) % 4;
-    if (Math.random() < 0.1) {
-      state = { type: "walk", x: 0 };
-    }
-    setTimeout(refresh, 500);
-  }
-
-  function standAnimation() {
-    if (state.t === undefined) state.t = 0;
-    if (state.t < 4) drawArt(20 + state.x, 30, standingCatToLeft, true);
-    else drawArt(20 + state.x, 30, standingCatToLeftPose2, true);
-    state.t++;
-    if (state.t > 8) state.t -= 8;
-    state.x += 1;
-    setTimeout(refresh, 50);
-  }
-
-  function walkAnimation() {
-    if (state.t === undefined) state.t = 0;
-    if (state.dir === undefined) state.dir = "left";
-    const flip = state.dir === "right";
-    if (state.t < 4) drawArt(20 + state.x, 30, standingCatToLeft, flip);
-    else drawArt(20 + state.x, 30, standingCatToLeftPose2, flip);
-    state.t++;
-    if (state.t > 8) state.t -= 8;
-    state.x += state.dir === "left" ? -1 : 1;
-    if (state.dir === "left" && state.x < -10) state.dir = "right";
-    if (state.dir === "right" && state.x > 10) state.dir = "left";
-    if (state.x === 0) {
-      if (Math.random() < 0.5) state = { type: "sleep" };
-    }
-    setTimeout(refresh, 100);
-  }
-
-  function easeInOut(t) {
-    const sqt = t ** 2;
-    return sqt / (2.0 * (sqt - t) + 1.0);
-  }
-
-  var colorT = 0;
-  var elem;
-  function setColor() {
-    if (colorT === undefined) colorT = 0;
-    if (!elem) elem = document.querySelector("#main-tamago");
-    const blueRGB = [98, 144, 241];
-    const orangeRGB = [241, 195, 98];
-    let t = colorT;
-    const incAm = 0.05;
-    if (state.type === "sleep") colorT = Math.min(t + incAm, 1);
-    else colorT = Math.max(t - incAm, 0);
-    t = easeInOut(t);
-    const rgbOut = colorTweenRGB(orangeRGB, blueRGB, t);
-    if (elem) elem.style.fill = `rgb(${rgbOut.join(",")})`;
-    document.body.style.backgroundColor = `rgb(${colorTweenRGB(
-      [20, 20, 50],
-      [98, 144, 241],
-      1 - t
-    ).join(",")})`;
-  }
-
-  setInterval(setColor, 30);
-  refresh();
-
-  function colorTweenRGB(a, b, t) {
-    return a.map((av, i) => (b[i] - av) * t + av);
-  }
-};
+.btn:active {
+  box-shadow: -0.5px -0.5px 0px 2px #00000033;
+}
